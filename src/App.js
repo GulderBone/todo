91253e6b827d917f37bd639e3d935b09b3beccb1
gulderbone <kira.gulderbone@gmail.com>
2019-03-16 16:52:13 +0100

    Add todo component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import TodosList from './components/TodosList';
+import AddTodo from './components/AddTodo';
 import './App.css';
 
 class App extends Component {
@@ -40,7 +41,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header></Header>
+        <Header />
+        <AddTodo />
         <TodosList todosList={this.state.todos}
         toggleComplete={this.toggleComplete} 
         delTodo={this.delTodo} />
